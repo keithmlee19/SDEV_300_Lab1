4c8b2a1a3b137d9f90270629ce7a58213848c17e
Keith Lee <keithmlee19@gmail.com>
2023-05-21 17:42:32 -0400

    Third pass, hopefully fixed trailing whitespaces, got rid of general error handling function, changed Exceptions to ValueError

--- SDEV_300_Lab1/src/voter_app.py
+++ SDEV_300_Lab1/src/voter_app.py
@@ -1,22 +1,17 @@
 '''Program to simulate a voter eligibility/registration application'''
 import sys
 
-def handlefun(i):
-    '''function that handles invalid inputs'''
-    pass
-
 def continue_reg():
     ''' function to ask user if they want to continue registration'''
     while True:
         try:
             user_input = input("Do you want to continue with voter registration?\n")
             if user_input.lower() == "yes":
-                handlefun(user_input)
                 break
             print("You've indicated that you want to stop. Thanks and goodbye")
-        except Exception:
+        except ValueError:
             pass
-        print("Invalid input, please enter yes or no")        
+        print("Invalid input, please enter yes or no")       
 print("Welcome to the Python voter registration application.")
 continue_reg()
 fname = input("What is your first name?\n")
@@ -26,10 +21,9 @@ while True: # validate age
     try:
         age = int(input("What is your age?\n"))
         if age < 120:
-            handlefun(age)
             break
-    except Exception:
-        pass    
+    except ValueError:
+        pass
     print("Invalid age input, please try again")
     continue_reg()
 continue_reg()
@@ -37,9 +31,8 @@ while True:
     try:
         citizen = input("Are you a U.S. citizen?\n")
         if citizen.lower() == "yes" or citizen.lower() == "no":
-            handlefun(citizen)
             break
-    except Exception:
+    except ValueError:
         pass
     print("Invalid input, please enter yes or no")
     continue_reg()
@@ -54,10 +47,9 @@ if age >= 18 and citizen.lower() == "yes":
             'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT',
             'VI', 'VA', 'WA', 'WV', 'WI', 'WY']
             if state in state_arr:
-                handlefun(state)
                 break
-        except Exception:
-            pass        
+        except ValueError:
+            pass     
         print("Sorry, that was not a valid abbreviation. Please try again.")
         continue_reg()
 else:
@@ -67,10 +59,9 @@ while True: # validate zip code, lowest zip code is 00501 and highest is 99950
     try:
         zipcode = input("What is your zipcode?\n")
         if int(zipcode) in range(501,99951) and len(zipcode) == 5:
-            handlefun(zipcode)
             break
-    except Exception:
-        pass    
+    except ValueError:
+        pass
     print("Invalid zip code, please try again")
     continue_reg()
 print("Thanks for registering to vote. Here is the information we received:")
